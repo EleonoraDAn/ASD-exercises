08db04050720ea614dd074692ff71560ef15784e
eleonoradan <eleonorayotobiana@gmail.com>
2026-04-27 17:30:54 +0200

    Modifica a 'DivideEtImpera.hpp'

--- algoritmi_divide_et_impera/main.cpp
+++ algoritmi_divide_et_impera/main.cpp
@@ -11,7 +11,11 @@ int main() {
     std::cout << sum << std::endl;
     int x = 4;
     bool result = ricercaBinaria(a, x, 0, n);
-    std::cout << result << std::endl;
+    if(result == 1) {
+        std::cout << x << " e' stato trovato!\n";
+    } else {
+        std::cout << x << " non e' stato trovato\n";
+    }
 
     return 0;
 }
